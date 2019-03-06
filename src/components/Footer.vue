e2aa5d4053ef83f0bb740f002a6814d4e8246d56
callmeSamZ <690236240@qq.com>
2019-03-07 01:30:25 +0800

    first

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="footer">
+    <span>writter by {{this.author}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      author: 'Sam Zhou'
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  #footer
+    margin-top 40px
+    text-align center
+    color #bfbfbf
+    font-size 10px
+    text-shadow 0px 1px 0px 1px
+</style>
